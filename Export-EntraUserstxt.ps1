26f245f26728599492b2d3839c91f117655810e8
yarina <yarinamihaelailie@gmail.com>
2026-09-02 15:19:56 +0300

    Add script to export Entra ID users to TXT file
    This script connects to Microsoft Graph to retrieve and export Entra ID users' details to a TXT file.

---
+++ Export-EntraUserstxt.ps1
@@ -0,0 +1,24 @@
+<#
+.synopsis
+    Exports all Entra ID users to a TXT file.
+
+.desc
+    Connects to Microsoft Graph using read-only permissions,
+    retrieves all users from Microsoft Entra ID, and saves
+    selected user properties to a text file.
+
+.note
+    Required Microsoft Graph permission:
+    User.Read.All
+#>
+
+Connect-MgGraph -Scopes "User.Read.All"
+
+$users = Get-MgUser -All -Property DisplayName,UserPrincipalName,Mail,AccountEnabled
+
+$users |
+    Select-Object DisplayName, UserPrincipalName, Mail, AccountEnabled |
+    Format-Table -AutoSize |
+    Out-File "C:\Temp\EntraUsers.txt"
+
+    
